da38b3d96e2fbd3621dcb2525e90210b79feaac0
Ekaterina Levshina <levshina@tutu.ru>
2020-05-11 17:01:43 +0300

    hw05_parallel_execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,86 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var ErrRoutinesCount = errors.New("routines count should be more than 0")
 
 type Task func() error
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
-	// Place your code here
+	err := validateParams(N, M)
+
+	if err != nil {
+		return err
+	}
+
+	var errorsCount = 0
+	var wg = sync.WaitGroup{}
+	var m = sync.Mutex{}
+	var tasksCount = len(tasks)
+
+	if N > tasksCount {
+		N = tasksCount
+	}
+
+	wg.Add(N)
+
+	tasksCh := make(chan Task, tasksCount)
+	for _, task := range tasks {
+		tasksCh <- task
+	}
+	close(tasksCh)
+
+	for i := 0; i < N; i++ {
+		go func() {
+			defer wg.Done()
+
+			for {
+				task, ok := <-tasksCh
+
+				if !ok {
+					break
+				}
+
+				err := task()
+				m.Lock()
+
+				if err != nil {
+					errorsCount++
+				}
+
+				if errorsCount == M {
+					m.Unlock()
+					return
+				}
+				m.Unlock()
+			}
+
+		}()
+	}
+
+	wg.Wait()
+
+	if errorsCount >= M {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+func validateParams(N int, M int) error {
+	var err error
+
+	if N <= 0 {
+		err = ErrRoutinesCount
+	}
+
+	if M <= 0 {
+		err = ErrErrorsLimitExceeded
+	}
+
+	return err
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -63,4 +63,64 @@ func TestRun(t *testing.T) {
 		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("wrong goroutines count", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		maxErrorsCount := 10
+
+		workersCount := 0
+		result1 := Run(tasks, workersCount, maxErrorsCount)
+		require.Equal(t, ErrRoutinesCount, result1)
+
+		workersCount = -10
+		result2 := Run(tasks, workersCount, maxErrorsCount)
+		require.Equal(t, ErrRoutinesCount, result2)
+	})
+
+	t.Run("tasks with errors less then M", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		maxErrorsCount := 25
+		errorsCount := 0
+		for i := 0; i < tasksCount; i++ {
+			if errorsCount < (maxErrorsCount - 5) {
+				tasks = append(tasks, func() error {
+					atomic.AddInt32(&runTasksCount, 1)
+					return fmt.Errorf("error from task %d", i)
+				})
+			} else {
+				tasks = append(tasks, func() error {
+					atomic.AddInt32(&runTasksCount, 1)
+					return nil
+				})
+			}
+			errorsCount++
+		}
+
+		workersCount := 10
+
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.NotEqual(t, ErrErrorsLimitExceeded, result)
+		require.LessOrEqual(t, runTasksCount, int32(tasksCount), "all tasks were started")
+	})
 }
