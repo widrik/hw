9b0f053923a6faa623f9e4efe5c2b59f07d3e921
Ekaterina Levshina <levshina@tutu.ru>
2020-06-18 20:43:36 +0300

    hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -10,6 +14,46 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	oldFile, err := os.Open(fromPath)
+
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer oldFile.Close()
+
+	fileInfo, err := oldFile.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case fileInfo.IsDir():
+		return ErrUnsupportedFile
+	case fileInfo.Size() < offset:
+		return ErrOffsetExceedsFileSize
+	default:
+	}
+
+	if _, err := oldFile.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	count := int(fileInfo.Size() - offset)
+	bar := pb.StartNew(count)
+	defer bar.Finish()
+
+	barReader := bar.NewProxyReader(oldFile)
+	_, err = io.CopyN(newFile, barReader, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
