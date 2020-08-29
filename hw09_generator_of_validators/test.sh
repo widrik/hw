42d978d3868c93841fc45a5a4c744fc06947ed0b
Ekaterina Levshina <levshina@tutu.ru>
2020-08-29 12:09:59 +0300

    Свежие изменения из репозитория #!/usr/bin/env bash

--- hw09_generator_of_validators/test.sh
+++ hw09_generator_of_validators/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
 rm -f "$(command -v go-validate)"
 rm -f ./models/*generated.go
@@ -8,5 +8,4 @@ go install ./go-validate
 go generate models/models.go
 go test -v -tags generation ./models
 
-rm -f go-validate
 echo "PASS"
